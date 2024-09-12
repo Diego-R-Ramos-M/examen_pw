93a49c6b66ff77e741f07e1c2371f8355f0ab5a8
O23406 <O23406@lab.aca>
2024-09-11 21:50:26 -0500

    pregunta3_ultimo_avance

---
+++ pregunta3.js
@@ -0,0 +1,11 @@
+let productos = [
+    {nombre : "Leche", cantidad : 4, precio : 2.5} ,
+    {nombre : "Gaseosa", cantidad : 2, precio : 5.3},
+    {nombre : "Galletas", cantidad : 7, precio: 1.20}
+]
+
+function main() {
+
+}
+
+main()
